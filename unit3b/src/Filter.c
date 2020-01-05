0025b2666073fe6240891b0dbd9e2d52bb570a2b
Pranata Wijaya <pranata.swijaya@gmail.com>
2020-01-05 18:29:43 +0700

    Add implementation on filter and test cases for unit3b

--- unit3b/src/Filter.c
+++ unit3b/src/Filter.c
@@ -1,2 +1,11 @@
+#include <stdint.h>
+
 #include "Filter.h"
 #include "MK20DX256.h"
+
+
+uint16_t Filter_AddVal(uint16_t prev_val, uint16_t new_val)
+{
+    uint32_t product = (uint32_t)prev_val * 3;
+    return (uint16_t)((product + new_val)/4);
+}
--- unit3b/src/Filter.h
+++ unit3b/src/Filter.h
@@ -1,7 +1,7 @@
 #ifndef FILTER_H
 #define FILTER_H
 
-
+uint16_t Filter_AddVal(uint16_t prev_val, uint16_t new_val);
 
 
 #endif //FILTER_H
